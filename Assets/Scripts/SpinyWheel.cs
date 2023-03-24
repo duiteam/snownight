f7d09b1fedda465cda3073232b523c38e6cf850b
XiangyuMeng1 <mengxi10@msu.edu>
2023-03-24 01:13:51 -0400

    更新了2-1；完成spinywheel

---
+++ Assets/Scripts/SpinyWheel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinyWheel : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public float limit = 75f;
+    public bool randomStart = false;
+    private float random = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (randomStart)
+            random = Random.Range(0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = limit * Mathf.Sin(Time.time + random * speed);
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+}
