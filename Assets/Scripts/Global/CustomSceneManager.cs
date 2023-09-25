d1504d78d37c88c81eaaba32de5cf1655fbef869
GalvinGao <me@galvingao.com>
2023-09-24 23:00:25 -0400

    fix: shadow and version mismatch save slot

--- Assets/Scripts/Global/CustomSceneManager.cs
+++ Assets/Scripts/Global/CustomSceneManager.cs
@@ -5,33 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class CustomSceneManager : MonoBehaviour
 {
-    public static CustomSceneManager Instance { get; private set; }
-    
     // blockSceneNames is a list of scene names that should not be saved
     public string[] blockSceneNames;
+    public static CustomSceneManager Instance { get; private set; }
 
     private void Awake()
     {
         if (Instance == null)
-        {
             Instance = this;
-            // DontDestroyOnLoad(gameObject);
-        }
+        // DontDestroyOnLoad(gameObject);
         else
-        {
             Destroy(gameObject);
-        }
+
+        GuardAgainstMismatchedGameVersion();
     }
 
     public void LoadScene(string sceneName)
     {
         StartCoroutine(DoLoadScene(sceneName));
     }
-    
+
     public void LoadScene(int sceneIndex)
     {
         StartCoroutine(DoLoadScene(sceneIndex));
     }
+
     private IEnumerator DoLoadScene(string sceneName)
     {
         yield return FindObjectOfType<LevelLoaderBehavior>().StartFadeOut();
@@ -39,7 +37,7 @@ public class CustomSceneManager : MonoBehaviour
         SaveScene(SceneManager.GetSceneByName(sceneName));
         yield return FindObjectOfType<LevelLoaderBehavior>().StartFadeIn();
     }
-    
+
     private IEnumerator DoLoadScene(int sceneIndex)
     {
         yield return FindObjectOfType<LevelLoaderBehavior>().StartFadeOut();
@@ -53,28 +51,42 @@ public class CustomSceneManager : MonoBehaviour
         if (Instance.blockSceneNames.Contains(scene.name)) return;
         PlayerPrefs.SetString("SavedSceneName", scene.name);
         PlayerPrefs.SetInt("SavedSceneIndex", scene.buildIndex);
+        PlayerPrefs.SetString("SavedGameVersion", Application.version);
         PlayerPrefs.Save();
     }
 
     public static string GetSavedSceneName()
     {
-        if (PlayerPrefs.HasKey("SavedSceneName"))
-        {
-            return PlayerPrefs.GetString("SavedSceneName");
-        }
+        if (PlayerPrefs.HasKey("SavedSceneName")) return PlayerPrefs.GetString("SavedSceneName");
 
         Debug.LogWarning("No saved scene found!");
         return null;
     }
-    
+
     public static int GetSavedSceneIndex()
     {
-        if (PlayerPrefs.HasKey("SavedSceneIndex"))
-        {
-            return PlayerPrefs.GetInt("SavedSceneIndex");
-        }
+        if (PlayerPrefs.HasKey("SavedSceneIndex")) return PlayerPrefs.GetInt("SavedSceneIndex");
 
         Debug.LogWarning("No saved scene found!");
         return -1;
+    }
+
+    public static string GetSavedGameVersion()
+    {
+        if (PlayerPrefs.HasKey("SavedGameVersion")) return PlayerPrefs.GetString("SavedGameVersion");
+
+        Debug.LogWarning("No saved game version found!");
+        return null;
+    }
+
+    private static void GuardAgainstMismatchedGameVersion()
+    {
+        if (GetSavedGameVersion() == Application.version) return;
+
+        Debug.LogWarning("Saved game version mismatch!");
+        PlayerPrefs.DeleteKey("SavedSceneName");
+        PlayerPrefs.DeleteKey("SavedSceneIndex");
+        PlayerPrefs.DeleteKey("SavedGameVersion");
+        PlayerPrefs.Save();
     }
 }
--- Assets/Scripts/Puzzle/PuzzlePieceController.cs
+++ Assets/Scripts/Puzzle/PuzzlePieceController.cs
@@ -1,19 +1,21 @@
 using System.Collections.Generic;
-using UnityEngine.EventSystems;
-using UnityEngine;
+using System.Linq;
 using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PuzzlePieceController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+public class PuzzlePieceController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler,
+    IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     public PuzzleController puzzleController;
-    public PuzzlePiece puzzlePiece;
     public CanvasGroup glowCanvasGroup;
-    
-    private Vector2 m_OriginalPosition;
-    private RectTransform m_RectTransform;
     private CanvasGroup m_CanvasGroup; // To control visibility and raycast target during drag
     private bool m_IsDragging;
-    
+
+    private Vector2 m_OriginalPosition;
+    private RectTransform m_RectTransform;
+    public PuzzlePiece puzzlePiece;
+
 
     private void Awake()
     {
@@ -31,18 +33,16 @@ public class PuzzlePieceController : MonoBehaviour, IBeginDragHandler, IDragHand
     {
         if (!enabled)
             return;
-        
+
+        if (m_IsDragging)
+            return;
+
         m_IsDragging = true;
         puzzleController.activeDragCount++;
         m_OriginalPosition = m_RectTransform.anchoredPosition;
         m_CanvasGroup.blocksRaycasts = false;
-        
+
         transform.SetAsLastSibling();
-    }
-    
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        glowCanvasGroup.DOFade(0.7f, 0.15f);
     }
 
     // During drag
@@ -50,19 +50,26 @@ public class PuzzlePieceController : MonoBehaviour, IBeginDragHandler, IDragHand
     {
         if (!enabled)
             return;
-        
+
+        if (!m_IsDragging)
+            return;
+
         // Convert the mouse's global position to a position relative to the PuzzleController's RectTransform
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(puzzleController.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out Vector2 localPoint))
-        {
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(puzzleController.GetComponent<RectTransform>(),
+                eventData.position, eventData.pressEventCamera, out var localPoint))
             m_RectTransform.anchoredPosition = localPoint;
-        }
     }
 
     // When ending the drag
     // When ending the drag
     public void OnEndDrag(PointerEventData eventData)
     {
-        m_IsDragging = false;
+        if (!enabled)
+            return;
+
+        if (!m_IsDragging)
+            return;
+
         puzzleController.activeDragCount--;
         m_CanvasGroup.blocksRaycasts = true;
 
@@ -70,57 +77,65 @@ public class PuzzlePieceController : MonoBehaviour, IBeginDragHandler, IDragHand
         var hitObjects = new List<RaycastResult>();
         EventSystem.current.RaycastAll(eventData, hitObjects);
 
-        PuzzlePieceController targetPieceController = null;
+        var targetPieceController =
+            (from result in hitObjects
+                where result.gameObject != gameObject && result.gameObject.GetComponent<PuzzlePieceController>()
+                select result.gameObject.GetComponent<PuzzlePieceController>()).FirstOrDefault();
 
         // Loop through hit objects to find the first PuzzlePieceController that's not this one
-        foreach (var result in hitObjects)
-        {
-            if (result.gameObject != gameObject && result.gameObject.GetComponent<PuzzlePieceController>())
-            {
-                targetPieceController = result.gameObject.GetComponent<PuzzlePieceController>();
-                break;
-            }
-        }
 
-        if (targetPieceController != null)
+        Debug.Log(
+            $"targetPieceController: {targetPieceController}; m_IsDragging: {targetPieceController?.m_IsDragging}");
+        if (targetPieceController != null && !targetPieceController.m_IsDragging)
         {
             // Swap positions with tween
             m_RectTransform.DOAnchorPos(targetPieceController.m_RectTransform.anchoredPosition, 0.3f);
-            targetPieceController.m_RectTransform.DOAnchorPos(m_OriginalPosition, 0.3f);
-            
+
+            targetPieceController.m_IsDragging = true;
+            targetPieceController.m_RectTransform.DOAnchorPos(m_OriginalPosition, 0.3f).onComplete +=
+                () =>
+                {
+                    m_IsDragging = false;
+                    targetPieceController.m_IsDragging = false;
+                };
+
             // swap puzzlePiece.currentPosition with targetPieceController.puzzlePiece.currentPosition
             (puzzlePiece.currentPosition, targetPieceController.puzzlePiece.currentPosition) =
                 (targetPieceController.puzzlePiece.currentPosition, puzzlePiece.currentPosition);
 
             targetPieceController.transform.SetAsLastSibling();
             transform.SetAsLastSibling();
+
+            glowCanvasGroup.DOFade(0, 0.15f);
+            targetPieceController.glowCanvasGroup.DOFade(0, 0.15f);
         }
         else
         {
             // If released outside the puzzle area, tween the piece back to its original position
-            m_RectTransform.DOAnchorPos(m_OriginalPosition, 0.3f);
+            m_RectTransform.DOAnchorPos(m_OriginalPosition, 0.3f).onComplete += () => m_IsDragging = false;
         }
-        
+
         // Check if the puzzle is solved
         puzzleController.TriggerIsSolvedDetection();
     }
-    
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        glowCanvasGroup.DOFade(0.7f, 0.15f);
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (!enabled)
             return;
         glowCanvasGroup.DOFade(1, 0.15f);
-        
+
         if (puzzleController.activeDragCount <= 0)
             transform.SetAsLastSibling();
     }
-    
+
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (!m_IsDragging)
-        {
-            glowCanvasGroup.DOFade(0, 0.15f);
-        }
+        if (!m_IsDragging) glowCanvasGroup.DOFade(0, 0.15f);
     }
-
 }
